b439736b589a23a31b6425dc829a3b26142a8e92
GokceSoylu <gokcesoylu24@gmail.com>
2024-12-24 08:48:28 +0300

    DONE

--- cps_f1/src/main.cpp
+++ cps_f1/src/main.cpp
@@ -1,60 +1,30 @@
-// #include <Arduino.h>
-
-// // put function declarations here:
-// int myFunction(int, int);
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   int result = myFunction(2, 3);
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
-
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
+#include <Arduino.h>
 #include <BLEMouse.h>
 
-// BLE Mouse nesnesini oluştur
-BLEMouse bleMouse;
+// BLE fare tanımı
+BleMouse bleMouse;
 
 void setup()
 {
+  // Seri iletişim başlatma
   Serial.begin(115200);
+  Serial.println("BLE Mouse başlatılıyor...");
 
-  // BLE Mouse'u başlat
-  if (!bleMouse.isConnected())
-  {
-    Serial.println("BLE Mouse başlatılıyor...");
-    bleMouse.begin();
-  }
+  // BLE fareyi başlat
+  bleMouse.begin();
 }
 
 void loop()
 {
-  // BLE bağlıysa fare hareketi ve tıklama simüle edelim
   if (bleMouse.isConnected())
   {
     Serial.println("BLE bağlı, fare hareketi yapılıyor...");
-
-    // Fareyi sağa doğru hareket ettir
-    bleMouse.move(10, 0);
-    delay(500);
-
-    // Sol fare tıklaması
-    bleMouse.click(MOUSE_LEFT);
+    bleMouse.move(10, 0); // Farenin sağa hareketi
     delay(500);
   }
   else
   {
     Serial.println("BLE bağlantısı bekleniyor...");
+    delay(1000);
   }
-
-  delay(1000);
 }
